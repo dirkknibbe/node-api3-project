9916988c9b31c76a3fa9941b92c2ef31c6dede75
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-03-09 21:06:17 -0700

    ok woo got all test passing

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,69 @@
+const Users = require("../users/users-model.js");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  const url = req.originalUrl;
+
+  console.log(`[${timestamp}] ${method} to ${url}}]`);
+  next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+
+  let id = req.params.id;
+  const bad_id_message = { message: "user not found" };
+
+  if (id == null) {
+    res.status(400).json(bad_id_message);
+    return;
+  }
+
+  let user;
+
+  try {
+    id = Number(id);
+    user = await Users.getById(req.params.id);
+    if (user == null) {
+      res.status(404).json(bad_id_message);
+      return;
+    }
+  } catch (err) {
+    res.status(500).json({ message: "problem finding user" });
+    return;
+  }
+
+  req.user = user;
+
+  next();
 }
 
-function validateUser(req, res, next) {
+async function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  let name = req.body.name;
+  const err_message = { message: "missing required name field" };
+
+  if (!name) {
+    res.status(400).json(err_message);
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+
+  const post = req.body.text;
+  const err_message = { message: "missing required text field" };
+
+  if (!post) {
+    res.status(400).json(err_message);
+  } else {
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost };
--- api/server.js
+++ api/server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const {
+  logger,
+  validatePost,
+  validateUser,
+  validateUserId,
+} = require("./middleware/middleware");
 
 const server = express();
 
@@ -8,7 +14,11 @@ const usersRouter = require("./users/users-router.js");
 server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
-server.use("/api/users/users-router", usersRouter);
+server.use("/api/users/", usersRouter);
+server.use(logger);
+server.use(validatePost);
+server.use(validateUser);
+server.use(validateUserId);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
